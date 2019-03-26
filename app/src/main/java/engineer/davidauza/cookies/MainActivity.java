647a52d48b71f9965538d5dfd4b63c63801b9f75
davidauza.engineer <davidauza01@gmail.com>
2019-03-26 09:37:09 -0500

    Practice done.

--- app/src/main/java/engineer/davidauza/cookies/MainActivity.java
+++ app/src/main/java/engineer/davidauza/cookies/MainActivity.java
@@ -1,7 +1,11 @@
 package engineer.davidauza.cookies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the cookie should be eaten.
+     */
+    public void eatCookie(View view) {
+        // Find a reference to the ImageView in the layout. Change the image.
+        ImageView imageView = findViewById(R.id.android_cookie_image_view);
+        imageView.setImageResource(R.drawable.after_cookie);
+
+        // Find a reference to the TextView in the layout. Change the text.
+        TextView textView = findViewById(R.id.status_text_view);
+        textView.setText(getString(R.string.main_full));
     }
 }
